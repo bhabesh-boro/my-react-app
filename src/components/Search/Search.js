9094b8232086c01470ce44fb5d9f8227c913bcab
Bhabesh boro <xyzbhabesh12@gmail.com>
2020-08-09 11:50:17 +0530

    details-ui-update

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Search.css';
+import React from "react";
+import "./Search.css";
 
 export default function Search(props) {
   const value = props.value;
@@ -8,8 +8,18 @@ export default function Search(props) {
 
   return (
     <div className="background">
-      <input className="search clear-margin-top" type="text" value={value} onChange={handleChange} />
-      <button className="btn btn-success clear-margin-top" onClick={handleSubmit}>Search</button>
+      <input
+        className="search clear-margin-top"
+        type="text"
+        value={value}
+        onChange={handleChange}
+      />
+      <button
+        className="btn btn-success clear-margin-top"
+        onClick={handleSubmit}
+      >
+        Search
+      </button>
     </div>
-  )
+  );
 }
--- src/pages/DetailsPage/DetailsPage.js
+++ src/pages/DetailsPage/DetailsPage.js
@@ -1,14 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function DetailsPage() {
-    const params = useParams();
-    /**
-     * Call server with imdbID
-     * URL Format : http://localhost:3001/omdb/imdb/tt0944947
-     * const imdbID = params.imdbID;
-     */
-    return (
-       <p> Details page : Imdb ID {params.imdbID}</p>
-    )
+  const params = useParams();
+  const [details, setDetails] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get("http://www.omdbapi.com/?apikey=67be9b43&i=tt1905041")
+      .then((res) => {
+        setDetails(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+  /**
+   * Call server with imdbID
+   * URL Format : http://localhost:3001/omdb/imdb/tt0944947
+   * const imdbID = params.imdbID;
+   */
+  return (
+    <div>
+      <p> Details page : Imdb ID {params.imdbID}</p>
+      <p>{details && details.Title}</p>
+
+      <p>{details && details.Actors}</p>
+      <p>{details && details.Awards}</p>
+      <p>{details && details.Title}</p>
+      <p>{details && details.Title}</p>
+      <p>{details && details.Title}</p>
+    </div>
+  );
 }
