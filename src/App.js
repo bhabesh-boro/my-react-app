1c2983c720103436aba5f4b3dd28f7bd3cc0a0b1
bhabesh-boro <xyzbhabesh12@gmail.com>
2020-08-09 10:01:55 +0530

    upadated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import './App.css';
+import SearchPage from './pages/SearchPage/SearchPage';
+import DetailsPage from './pages/DetailsPage/DetailsPage';
+import HomePage from './pages/HomePage/HomePage';
 
+/**
+ * The Router maps a url to a component
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid remove-padding">
+      <Router>
+        <Switch>
+          <Route path="/search" component={SearchPage} />
+          <Route path="/details/:imdbID" component={DetailsPage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+
+const baseUrl = 'http://localhost:8000/';
+const axiosHeaders = {
+	headers: {
+	  'Access-Control-Allow-Origin': '*',
+	}
+};
+export { baseUrl, axiosHeaders }
